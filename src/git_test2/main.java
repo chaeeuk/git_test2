483edf63d355b053233f502a87cfd95bc876ea55
chaeeuk <chaehusla@gmail.com>
2026-04-08 10:06:29 +0900

    화면 출력 방법 수정

--- src/git_test2/main.java
+++ src/git_test2/main.java
@@ -1,10 +1,10 @@
-package git_test2;
+package git_test2; 
 
-public class main {
-
-	public static void main(String[] args) {
-		Hello h = new Hello();
-		h.say();
-	}
+public class Main { 
 	
+     public static void main(String[] args) { 
+        Hello hello = new Hello(); 
+        System.out.println(hello.say()); 
+    } 
+
 }
